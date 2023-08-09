daeba7559a33f6a516f1bfa52d1f99e1c509f9dc
Juan José Restrepo Rosero <49797614+JuanjoRestrepo@users.noreply.github.com>
2023-08-08 19:07:32 -0500

    Add files via upload

---
+++ Primeros_Pasos/src/PrimeraClase.java
@@ -0,0 +1,12 @@
+
+public class PrimeraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte edad;
+		edad = 22;
+		System.out.println("Edad Juan: " + edad);
+	}
+
+}
+	
